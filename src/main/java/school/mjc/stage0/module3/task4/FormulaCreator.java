53b2706154f30f5ee2cf355ea9fe7ff0c2eb0f12
joel912 <joelhehehe1234@gmail.com>
2025-05-23 10:54:47 +0530

    Update FormulaCreator.java

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        int a = 2;
+        int b= 5;
+
+        int a1 = (9 *a * a  - 5 *b  +2  +a -7);
+        int a2 = (a+b - 4*a*b)/2;
+
+        int res = a1 *a2;
+        System.out.println(res);
     }
 }
